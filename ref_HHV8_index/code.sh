3a28b7e6189bf58dfbea8c1b139ac811190ce847
abdeljalil-senhaji <abdeljalil.senhajirachik@usmba.ac.ma>
2025-06-19 09:03:04 +0000

    new project RNAseq

---
+++ ref_HHV8_index/code.sh
@@ -0,0 +1,7 @@
+STAR \
+  --runThreadN 4 \
+  --genomeDir hhv8_star_index \
+  --genomeFastaFiles GCF_000826975.1_ASM82697v1_genomic.fna \
+  --sjdbGTFfile GCF_000826975.1_ASM82697v1_genomic.gtf \
+  --sjdbOverhang 100 \
+  --runMode genomeGenerate
