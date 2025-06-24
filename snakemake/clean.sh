43032eb11071afca4620de386deffffadc0dc78c
abdeljalil-senhaji <abdeljalil.senhajirachik@usmba.ac.ma>
2025-06-24 09:35:36 +0000

    Sauvegarde auto 2025-06-24 09:35

---
+++ snakemake/clean.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+## clean
+
+
+
+RESULTSPATH='/scratch/recherche/asenhaji/projet_HHV8/pipeline_analysis/snakemake/output'
+
+
+
+rm -r $RESULTSPATH/*
+rm -r $RESULTSPATH/.snakemake
